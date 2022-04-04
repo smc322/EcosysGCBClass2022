8661baba519f4ab864f105c2232f5452c852d745
LinneaRock <lrock@wisc.edu>
2022-04-04 15:30:05 -0600

    LAR: updated hydro/chemograph function and code to create graphs with hydro, chemo lines on same graph.

--- Hydro_Chemo_Graphs/Code/Functions/chemo_hydrograph.R
+++ Hydro_Chemo_Graphs/Code/Functions/chemo_hydrograph.R
@@ -1,8 +1,8 @@
 
 ## Function to create the hydrograph and chemograph
 
+
 chemo_hydrograph <- function(chemData, chemVar, name_units, disData) {
-  
   
   chemData.1 <- chemData |>
     mutate(chemVar = as.numeric(chemVar)) |>
@@ -12,24 +12,62 @@ chemo_hydrograph <- function(chemData, chemVar, name_units, disData) {
   mindate <- min(chemData.1$date)
   maxdate <- max(chemData.1$date)
   
-  a <- ggplot(chemData.1) +
-    geom_line(aes(date, chemVar)) +
+  coef <- mean(as.numeric(chemVar), na.rm = TRUE) / mean(disData$discharge_rate, na.rm = TRUE)
+  
+  
+  ggplot() +
+    geom_line(disData, mapping = aes(date, discharge_rate * coef), color = "#9A9391") +
+    geom_line(chemData.1, mapping = aes(date, chemVar), color = "#fb8472") +
     theme_light() +
     scale_x_date(limits = c(mindate, maxdate)) +
-    labs(x = "",
-         y = name_units)
-  
-  
-  
-  b <- ggplot(disData) +
-    geom_line(aes(date, discharge_rate)) +
-    theme_light() +
-    labs(x = "",
-         y = "Streamflow" ~ (m ^ 3 ~ s ^ -1)) +
-    scale_x_date(limits = c(mindate, maxdate))
-  
-  
-  library(cowplot)
-  plot_grid(a, b, align = "v", ncol = 1)
+    labs(x = "") +
+    scale_y_continuous(
+      # first axis
+      name = name_units,
+      
+      # second axis 
+      sec.axis = sec_axis(~./coef, name = "Streamflow" ~ (m ^ 3 ~ s ^ -1))
+    )  +
+    theme(axis.title.y.right = element_text(color = "#9A9391"),
+          axis.title.y = element_text(color = "#fb8472")) 
   
 }
+
+
+################################################################################
+#### Code to create hydro/chemographs that are stacked 
+
+
+#
+# chemo_hydrograph <- function(chemData, chemVar, name_units, disData) {
+#   
+#   
+#   chemData.1 <- chemData |>
+#     mutate(chemVar = as.numeric(chemVar)) |>
+#     filter(!is.na(chemVar))
+#   
+#   
+#   mindate <- min(chemData.1$date)
+#   maxdate <- max(chemData.1$date)
+#   
+#   a <- ggplot(chemData.1) +
+#     geom_line(aes(date, chemVar)) +
+#     theme_light() +
+#     scale_x_date(limits = c(mindate, maxdate)) +
+#     labs(x = "",
+#          y = name_units)
+#   
+#   
+#   
+#   b <- ggplot(disData) +
+#     geom_line(aes(date, discharge_rate)) +
+#     theme_light() +
+#     labs(x = "",
+#          y = "Streamflow" ~ (m ^ 3 ~ s ^ -1)) +
+#     scale_x_date(limits = c(mindate, maxdate))
+#   
+#   
+#   library(cowplot)
+#   plot_grid(a, b, align = "v", ncol = 1)
+#   
+# }
